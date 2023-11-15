10752d43f27a733f60130d598f7160b4beda8213
Argetuin <148481278+Argetuin@users.noreply.github.com>
2023-11-15 16:57:17 -0500

    Delete python.py

--- python.py
+++
@@ -1,9 +0,0 @@
-temperatura = float(input("Ingrese temperatura: "))
-temp = input("Es Fahrenheit (F) o Celcius (C): ").lower()
-
-if temp == "f":
-        operaciones = ((temperatura-32)*(5/9))
-        operaciones = round(operaciones, 2)
-        print (operaciones)
-    
-    
